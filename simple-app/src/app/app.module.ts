7e7bca0f302294baf0541e59247c8c6dec2d640e
dam <damien.chancerel@gmail.com>
2022-05-19 12:24:10 +0200

    clone

---
+++ simple-app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleService } from './article-http-rest-source.service';
+import { ArticleCreationComponent } from './article-creation/article-creation.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { AuthorComponent } from './author/author.component';
+import { TopArticlesComponent } from './top-articles/top-articles.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent,
+    ArticlesComponent,
+    ArticleCreationComponent,
+    ArticleDetailComponent,
+    AuthorComponent,
+    TopArticlesComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [ArticleService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ simple-app/src/app/top-articles/top-articles.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/Article';
+import { ArticleService } from '../article-http-rest-source.service';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './top-articles.component.html',
+  styleUrls: ['./top-articles.component.css']
+})
+export class TopArticlesComponent implements OnInit {
+
+  articles   !: Article[];
+  searchText ?: string;
+
+  constructor(private articleService: ArticleService) { }
+
+  ngOnInit() {
+    this.articleService.getTopTen().subscribe(value => {
+      this.articles = value;
+    });
+  }
+
+  delete(article: Article) {
+    this.articleService.deleteArticle(article.id).subscribe(value => {
+      this.articles = this.articles.filter(value => value.id !== article.id);
+    });
+  }
+
+  searchKeyword(e: Event) {
+    const keyword = (<HTMLInputElement>e.target).value;
+    
+    this.articleService.searchArticle(keyword).subscribe(value => {
+      this.articles = value;
+    });
+  }
+
+}
